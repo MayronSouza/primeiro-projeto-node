73c5e0d5af0d307689f1df68c20608881ad58259
MayronGMZ <mayron.souza@gmz.solutions>
2020-05-23 23:52:05 -0300

    Primeiro Commit

---
+++ src/server.ts
@@ -0,0 +1,7 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => res.json({ message:'Hello world' }))
+
+app.listen(3333,() => console.log('Server started on port 3333'))
